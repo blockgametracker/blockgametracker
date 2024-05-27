524baf7b98a0ac9fec18edeccd47e3ddd8d608e3
Anthony Teague <anthony.teague@noxcrew.com>
2024-05-27 16:14:44 +0100

    Fix server comparison

--- apps/web/components/graphs/graphServers.tsx
+++ apps/web/components/graphs/graphServers.tsx
@@ -1,10 +1,16 @@
 "use client"
 
 import React from "react"
-import { URLParams, isServerToggled } from "@/utils/urlBuilder"
+import {
+    URLParams,
+    buildURL,
+    isServerToggled,
+    toggleServer,
+} from "@/utils/urlBuilder"
 import { Icon } from "../icon"
 import { ServerData } from "@/utils/parsedData"
 import { ServerCardSmall } from "../server/serverCardSmall"
+import Link from "next/link"
 
 interface Props {
     urlParams: URLParams
@@ -15,18 +21,29 @@ export const GraphServers = ({ urlParams, servers }: Props) => (
     <div className="col-span-6 tablet:col-span-1 w-full grid grid-cols-1 phone:grid-cols-2 tablet:grid-cols-4 normal:grid-cols-6 gap-4 overflow-y-scroll overflow-x-hidden">
         {servers.map((server) => {
             const active = isServerToggled(urlParams, server)
+            const newServers = toggleServer(urlParams, server)
 
             return (
-                <ServerCardSmall
-                    server={server}
+                <Link
+                    href={buildURL(
+                        urlParams.rangeParams,
+                        urlParams.edition,
+                        newServers,
+                        null,
+                    )}
                     key={`server-card-${server.server_slug}`}
                 >
-                    <div
-                        className={`w-4 h-4 ml-auto rounded-md border-2 flex items-center justify-center ${active ? "bg-mainText border-mainText" : "border-darkOverlay"}`}
-                    >
-                        <Icon iconName="check" className="w-4 h-4 fill-dark" />
-                    </div>
-                </ServerCardSmall>
+                    <ServerCardSmall server={server}>
+                        <div
+                            className={`w-4 h-4 ml-auto rounded-md border-2 flex items-center justify-center ${active ? "bg-mainText border-mainText" : "border-darkOverlay"}`}
+                        >
+                            <Icon
+                                iconName="check"
+                                className="w-4 h-4 fill-dark"
+                            />
+                        </div>
+                    </ServerCardSmall>
+                </Link>
             )
         })}
     </div>
