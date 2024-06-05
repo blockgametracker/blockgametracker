f41c88578110d4d3d37b94131f9c4062b8457710
Michael H. <michael@iptables.sh>
2024-06-05 21:55:43 +0200

    PORT env var support

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup("docs", app, document)
 
-    await app.listen(8080)
+    await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap()
