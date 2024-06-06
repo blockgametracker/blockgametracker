e2ccad5fc5b1dd0e8898f4e8efda09f2fce5e133
Anthony Teague <anthony.teague@noxcrew.com>
2024-06-06 19:10:05 +0100

    MChange to port 8080

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -15,6 +15,6 @@ async function bootstrap() {
     const document = SwaggerModule.createDocument(app, config)
     SwaggerModule.setup("docs", app, document)
 
-    await app.listen(process.env.PORT ?? 3000)
+    await app.listen(process.env.PORT ?? 8080)
 }
 bootstrap()
